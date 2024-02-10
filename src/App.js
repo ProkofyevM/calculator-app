bff78be6f1c3438b0220eccc83898c341c0cc9fa
Миша Прокофьев <misaprokofev@MacBook-Air--Misha.local>
2024-02-10 18:48:29 +0300

    calculator test1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Button = ({ number, onClick }) => {
+   console.log(number, onClick)
+   return (
+      <button onClick={onClick}>
+         {number}
+      </button>
+   )
 }
 
-export default App;
+export const App = () => {
+   const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+   const [operand1, setOperand1] = useState('')
+   const [operator, setOperator] = useState('')
+   const [operand2, setOperand2] = useState('')
+   const [isResult, setIsResult] = useState(false)
+
+   const handleClickOperand1 = num => {
+      if (operator) {
+         setOperand2(acc => acc + num)
+      } else {
+         setOperand1(acc => acc + num)
+      }
+   }
+
+   const handleClickOperator = operator => {
+      if (operand1) {
+         setOperator(operator)
+      }
+   }
+
+   const handleClickCancel = () => {
+      if (operand1 || operator || operand2) {
+         setOperand1('')
+         setOperator('')
+         setOperand2('')
+         setIsResult(false)
+      }
+   }
+
+   const calcResult =(op1, op, op2)=> {
+      const num1 = Number(op1)
+      const num2= Number(op2)
+
+      if(op === '+'){
+         return num1 + num2
+      }else if(op === '-'){
+         return num1 - num2
+      }
+   }
+
+   const handleClickEqual = () => {
+      if (operand2) {
+         const result = calcResult(operand1, operator, operand2);
+         setOperand1(result.toString())
+         setOperator("")
+         setOperand2("")
+         setIsResult(true)
+   }
+}
+
+   return (
+      <div className='app'>
+         <div className='calc'>
+            <input
+               type='text'
+               value={operand1 + operator + operand2}
+               className={isResult ? 'inp result' : 'inp'}
+            ></input>
+            <button className='btn1' onClick={() => handleClickCancel()}>
+               c
+            </button>
+            <button className='btn1' onClick={() => handleClickOperator('+')}>
+               +
+            </button>
+            <button className='btn1' onClick={() => handleClickOperator('-')}>
+               -
+            </button>
+            {numbers.map(number => (
+               <Button key = {number} number={number} onClick={() => handleClickOperand1(number)} />
+            ))}
+            <button className = 'btn1 btn'  onClick={handleClickEqual}>
+               =
+            </button>
+         </div>
+      </div>
+   )
+}
+
+
